c6cc8ef8226c5189f3b83b671c4c57d9791b25df
Marina Insaurralde <marinainsaurralde11@gmail.com>
2018-02-23 11:16:17 -0300

    Configuraciones y CRud de caja

---
+++ Jagua/Controllers/CajasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JaguaEntityModel;
+
+namespace Jagua.Controllers
+{
+    public class CajasController : Controller
+    {
+        private JaguaEntities db = new JaguaEntities();
+
+        // GET: Cajas
+        public ActionResult Index()
+        {
+            return View(db.caja.ToList());
+        }
+
+        // GET: Cajas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            caja caja = db.caja.Find(id);
+            if (caja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(caja);
+        }
+
+        // GET: Cajas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cajas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_caja,descripcion,id_usuario")] caja caja)
+        {
+            if (ModelState.IsValid)
+            {
+                db.caja.Add(caja);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(caja);
+        }
+
+        // GET: Cajas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            caja caja = db.caja.Find(id);
+            if (caja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(caja);
+        }
+
+        // POST: Cajas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_caja,descripcion,id_usuario")] caja caja)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(caja).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(caja);
+        }
+
+        // GET: Cajas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            caja caja = db.caja.Find(id);
+            if (caja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(caja);
+        }
+
+        // POST: Cajas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            caja caja = db.caja.Find(id);
+            db.caja.Remove(caja);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Jagua/Models/Caja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jagua.Models
+{
+    public class Caja
+    {
+        [Key]//propiedad que identifica que Id_caja es el PK
+        public int Id_caja { get; set; }
+        public string Descripcion { get; set; }
+        public int Id_usuario { get; set; }
+    }
+}
